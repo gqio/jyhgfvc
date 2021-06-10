c17fa3e02e90fe66f59644c4281833df960011f4
Gregory Quinio <1063530+gqio@users.noreply.github.com>
2021-06-10 16:43:32 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello TOTO!"/>
+    <HelloWorld msg="Hello TITI!"/>
   </div>
 </template>
 
